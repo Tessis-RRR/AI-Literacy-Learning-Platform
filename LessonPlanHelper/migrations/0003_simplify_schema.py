93fbae20f42d3c8de1636089cc989126f60246ab
ninilin2023 <147220966+ninilin2023@users.noreply.github.com>
2026-04-19 13:30:28 -0400

    Resolve merge conflict in requirements.txt

--- LessonPlanHelper/migrations/0003_simplify_schema.py
+++ LessonPlanHelper/migrations/0003_simplify_schema.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("LessonPlanHelper", "0002_analysis_views"),
+        ("LessonPlanHelper", "0001_initial"),
     ]
 
     operations = [
